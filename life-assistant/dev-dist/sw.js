d3ce9e49716c9e16454101d047b031aa5ca55ea3
RobotsBuildingEducation <robotsbuildingeducation@gmail.com>
2025-11-17 23:57:32 -0600

    store

--- life-assistant/dev-dist/sw.js
+++ life-assistant/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.55h8ol22c58"
+    "revision": "0.nm1el0ikhsg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
